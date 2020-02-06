caacb0ae6ca6d08826f06d49aa881d2f601ea367
Can Hanhan <p_github@finarfin.net>
2020-02-06 06:26:10 -0600

    Allow custom handler

--- common/tester.go
+++ common/tester.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -39,80 +40,100 @@ func (t *Tester) Close() {
 	}
 }
 
-func (tester *Tester) Setup(t *testing.T, category string, scenario string) {
+func (tester *Tester) Scenario(category string, scenario string) (*TestScenario, error) {
 	c, ok := tester.Categories[category]
 	if !ok {
-		t.Fatalf("Category %s was not found.", category)
+		return nil, fmt.Errorf("category %s was not found", category)
 	}
 
 	s, ok := c.Scenarios[scenario]
 	if !ok {
-		t.Fatalf("Scenario %s was not found in category %s.", scenario, category)
+		return nil, fmt.Errorf("scenario %s was not found in category %s", scenario, category)
+	}
+
+	return &s, nil
+}
+
+func (tester *Tester) Setup(t *testing.T, category string, scenario string) {
+	s, err := tester.Scenario(category, scenario)
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	tester.mux.HandleFunc(s.Request.Path, func(w http.ResponseWriter, req *http.Request) {
-		actualBody, err := ioutil.ReadAll(req.Body)
+		CompareRequests(t, &s.Request, req)
+		WriteResponse(t, &s.Response, w)
+	})
+}
+
+func (tester *Tester) Do(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	tester.mux.HandleFunc(pattern, handler)
+}
+
+func CompareRequests(t *testing.T, expected *TestRequest, actual *http.Request) {
+	actualBody, err := ioutil.ReadAll(actual.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, expected.Method, actual.Method)
+	for k, v := range expected.Headers {
+		assert.Contains(t, actual.Header, k)
+		assert.Equal(t, v, actual.Header[k][0])
+	}
+
+	if expected.Body != nil {
+		expectedBody, err := ioutil.ReadAll(expected.Body)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		assert.Equal(t, s.Request.Method, req.Method)
-		for k, v := range s.Request.Headers {
-			assert.Contains(t, req.Header, k)
-			assert.Equal(t, v, req.Header[k][0])
-		}
-
-		if s.Request.Body != nil {
-			expectedBody, err := ioutil.ReadAll(s.Request.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if cType, ok := s.Request.Headers["Content-Type"]; ok && cType == "application/json" {
-				var expectedData interface{}
-				if len(expectedBody) > 0 {
-					if err := json.Unmarshal(expectedBody, &expectedData); err != nil {
-						t.Fatal(err)
-					}
+		if cType, ok := expected.Headers["Content-Type"]; ok && cType == "application/json" {
+			var expectedData interface{}
+			if len(expectedBody) > 0 {
+				if err := json.Unmarshal(expectedBody, &expectedData); err != nil {
+					t.Fatal(err)
 				}
+			}
 
-				var actualData interface{}
-				if len(actualBody) > 0 {
-					if err := json.Unmarshal(actualBody, &actualData); err != nil {
-						t.Fatal(err)
-					}
+			var actualData interface{}
+			if len(actualBody) > 0 {
+				if err := json.Unmarshal(actualBody, &actualData); err != nil {
+					t.Fatal(err)
 				}
-
-				assert.Equal(t, expectedData, actualData)
-			} else {
-				assert.Equal(t, expectedBody, actualBody)
-			}
-		}
-
-		for k, v := range s.Response.Headers {
-			if k == "Content-Length" {
-				continue
 			}
 
-			w.Header().Add(k, v)
-		}
-
-		if s.Response.Body != nil {
-			resBody, err := ioutil.ReadAll(s.Response.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			resLen := strconv.Itoa(len(resBody))
-			w.Header().Add("Content-Length", resLen)
-			w.WriteHeader(s.Response.Code)
-
-			_, err = w.Write(resBody)
-			if err != nil {
-				t.Fatal(err)
-			}
+			assert.Equal(t, expectedData, actualData)
 		} else {
-			w.WriteHeader(s.Response.Code)
+			assert.Equal(t, expectedBody, actualBody)
 		}
-	})
+	}
+}
+
+func WriteResponse(t *testing.T, resp *TestResponse, w http.ResponseWriter) {
+	for k, v := range resp.Headers {
+		if k == "Content-Length" {
+			continue
+		}
+
+		w.Header().Add(k, v)
+	}
+
+	if resp.Body != nil {
+		resBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		resLen := strconv.Itoa(len(resBody))
+		w.Header().Add("Content-Length", resLen)
+		w.WriteHeader(resp.Code)
+
+		_, err = w.Write(resBody)
+		if err != nil {
+			t.Fatal(err)
+		}
+	} else {
+		w.WriteHeader(resp.Code)
+	}
 }
